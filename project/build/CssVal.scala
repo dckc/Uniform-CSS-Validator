2204c6b1e574ba287e2a1de7bc4b62c341cbee52
Dan Connolly <http://www.w3.org/People/Connolly/>
2009-11-09 14:40:44 -0600

    comment reason for using v 0.9.5 of scalatest

--- project/build/CssVal.scala
+++ project/build/CssVal.scala
@@ -2,7 +2,8 @@ import sbt.{ProjectInfo, DefaultProject}
 
 class CssVal(info: ProjectInfo) extends DefaultProject(info)
 {
-
+  /* There seem to be some version skew issues between sbt and ScalaTest,
+   * so we're using scalatest 0.9.5 for now. */
   val scalatest = "org.scalatest" % "scalatest" % "0.9.5" % "test->default"
 
 }
